d8016118f23229cf233364901610276cf68e064b
w-amon <xxx110680@gmail.com>
2023-10-10 10:13:52 +0300

    string.js

---
+++ string.js
@@ -0,0 +1,14 @@
+/*Create a JavaScript program that showcases explicit type conversion to a string. 
+Follow these instructions:
+1. Declare a variable `num` and assign it a numerical value, e.g., `2023`.
+2. Use explicit type conversion to convert `num` to a string using the 
+`String()` function.
+3. Display the converted string in the console.
+Provide comments to explain how you explicitly converted the number to a string 
+using the `String()` function.*/
+
+var num = 20; //Declare a variable(num) and assign it a numerical value(20)
+
+var convertedString = String(num); //explicit conversion of 'num' to string using the 'String()' function
+
+console.log("Converted String:", convertedString); //Display the converted string in the console
