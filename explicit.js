8bb5603229af8d9dc972ea22d0347987a09827b3
Amon <amon@Amons-MacBook-Air-3.local>
2023-10-10 10:00:41 +0300

    explicit.js

---
+++ explicit.js
@@ -0,0 +1,15 @@
+/* Write a JavaScript program that demonstrates explicit type conversion to a number.
+Perform the following steps:
+1. Declare a variable `numberStr` and assign it a string value, e.g., `"42"`.
+2. Use explicit type conversion to convert `numberStr` to a number using the 
+`Number()` function.
+3. Display the converted number in the console.
+Explain in comments how you explicitly converted the string to a number using the 
+`Number()` function.*/
+
+
+var numberStr = "20"; //Declare a variable (numberStr) and assign it a string value
+
+var convertedNumber = Number(numberStr); //Convert 'numberStr' to a number using the 'Number()' function
+
+console.log("Converted Number:", convertedNumber); //Display the converted number in the console
